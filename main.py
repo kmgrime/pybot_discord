9741d0596f4c30aeb31cbbc5e30323248abdaf3e
kmgrime <kmgrime@gmail.com>
2023-04-17 23:12:59 +0200

    first layout

--- main.py
+++ main.py
@@ -1,5 +1,42 @@
 #imports
 import discord
+import random
 
 # token
 TOKEN = 'test123YOURTOKENtest123'
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+    username = str(message.author).split('#')[0]
+    user_message = str(message.content)
+    channel = str(message.channel.name)
+    print(f'{username}: {user_message} ({channel})')
+
+    if message.author == client.user:
+        return
+    
+    #response to general channel
+    if message.channel.name == 'General':
+        if user_message.lower() == 'hello':
+            await message.channel.send(f'Hello {username}!')
+            return
+        elif user_message.lower() == 'bye':
+            await message.channel.send(f'Bye {username}!')
+            return
+        elif user_message.lower() == '!random':
+            response = f'This is your random number: {random.randrange(1000000)}'
+            await message.channel.send(response)
+            return
+    
+    #response to all channels    
+    if user_message.lower() == '!here'
+        await message.channel.send('Yes I am everywhere')
+
+
+client.run(TOKEN)
