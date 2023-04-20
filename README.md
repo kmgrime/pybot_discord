2f8b14ba36c0a0f115a9e5cc0c525374cb5323e8
Kjell-Magnus Grime <kjell-magnus.grime@iver.no>
2023-04-20 08:54:49 +0200

    add readme, cleanup, add jokes

--- README.md
+++ README.md
@@ -1,2 +1,45 @@
-# pybot
-Discord bot written in python
+# Discord Bot with Responses
+
+This is a Python script for a basic Discord bot that responds to specific commands or messages. The bot will respond to messages with certain keywords or commands, such as "hello" or "!roll". It also includes PyJokes package to add some humor to its responses.
+
+## Installation
+
+1. Clone this repository to your local machine.
+2. Install the necessary Python packages by running the following command:
+
+```sh
+
+pip install -r requirements.txt
+```
+
+1. Create a new Discord bot and obtain its token. You can follow the steps [here](https://discordpy.readthedocs.io/en/stable/discord.html) to create a bot and obtain its token.
+2. Replace the `TOKEN` variable in the code with your bot's token.
+
+## Usage
+
+To start the bot, run the following command in your terminal or command prompt:
+
+```sh
+
+python bot.py
+```
+
+The bot will now be online and listening for messages in the Discord server it's been added to. The bot responds to the following commands:
+
+- `!roll`: Generates a random number between 1 and 100.
+- `!help`: Displays the available commands.
+- `!joke`: Returns a random joke using the PyJokes package.
+- `hello`: Responds with "Hello there!".
+
+The bot also logs messages it receives, printing the author's username, message content, and channel it was sent in.
+
+## Contributing
+
+If you have any suggestions for improvements or new features to add to the bot, feel free to open an issue or submit a pull request.
+
+## Credits
+
+This bot uses the following Python packages:
+
+- `discord`: For creating the bot and interacting with the Discord API.
+- `pyjokes`: For generating random jokes to include in the bot's responses.
--- bot.py
+++ bot.py
@@ -1,21 +1,23 @@
-#imports
+# imports
 import discord
 import responses
 
 # response
-async def send_message(message, user_message, is_private):
+
+
+async def send_message(message, user_message):
     try:
         response = responses.get_response(user_message)
-        await message.author.send(response) if is_private else await message.channel.send(response)
-    
-    #bad exception - do better in next increment
+        await message.channel.send(response)
+
+    # bad exception - do better
     except Exception as e:
         print(e)
 
-# bot
+
 # remember to change token before pushing code to repo
 def run_discord_bot():
-    TOKEN = 'YOURETOKENHERE'
+    TOKEN = 'MTA5Nzc4ODM1MTk4NzMyNzAwNw.Gsm1i6.wcnSaO2BNvEsiLlZxO57rI9RTe47mGhxwTOPsU'
     intents = discord.Intents.default()
     intents.message_content = True
     client = discord.Client(intents=intents)
@@ -28,17 +30,20 @@ def run_discord_bot():
     async def on_message(message):
         if message.author == client.user:
             return
-        
+
         username = str(message.author)
         user_message = str(message.content)
         channel = str(message.channel)
 
         print(f'{username} said: "{user_message}" ({channel})')
 
-        if user_message[0] == '?':
+        if user_message[0] == '!':
+            print(user_message[0])
             user_message = user_message[1:]
-            await send_message(message, user_message, is_private=True)
+            await send_message(message, user_message)
+        elif user_message.lower() == 'hello':
+            await send_message(message, user_message)
         else:
-            await send_message(message, user_message, is_private=False)
-        
+            print('nothing done')
+
     client.run(TOKEN)
