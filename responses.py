0212b33b37c41934ad33513937d4976fb330384d
Kjell-Magnus Grime <kjell-magnus.grime@iver.no>
2023-04-20 10:15:13 +0200

    add jokes to help command

--- responses.py
+++ responses.py
@@ -5,14 +5,14 @@ import pyjokes
 def get_response(message: str) -> str:
     p_message = message.lower()
 
+    if p_message == 'help':
+        return '`Available commands: !help !roll !jokes and hello`'
+
     if p_message == 'hello':
         return 'Hello there!'
 
     if p_message == 'roll':
         return str(random.randint(1, 100))
-
-    if p_message == 'help':
-        return '`Available commands: !roll !help`'
 
     if p_message == 'joke':
         return pyjokes.get_joke()
